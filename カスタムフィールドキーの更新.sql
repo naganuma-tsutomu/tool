b60599cb2b807363d2d4b9b49655a36dc4389c10
naganuma-tsutomu <123545778+naganuma-tsutomu@users.noreply.github.com>
2023-02-08 18:22:42 +0900

    update

--- カスタムフィールドキーの更新.sql
+++ カスタムフィールドキーの更新.sql
@@ -1,5 +1,5 @@
-UPDATE `wp_postmeta` SET `meta_key`='_top_img' WHERE `meta_key`='_item_img';
-UPDATE `wp_postmeta` SET `meta_key`='top_img' WHERE `meta_key`='item_img';
--- カスタムフィールドのキー更新
+UPDATE `wp_postmeta` SET `meta_key`='{keyAfter}' WHERE `meta_key`='{keyBefore}';
+UPDATE `wp_postmeta` SET `meta_key`='_{keyAfter}' WHERE `meta_key`='_{keyBefore}';
+-- カスタムフィールドのキー更新 '_'付きも更新しなければ反映されない
 -- SETは更新後
 -- WHEREは更新前
